5ae679ee438c0d787d13c58532c2a7db1d8b9990
EC2 Default User <ec2-user@ip-172-31-46-70.ap-northeast-1.compute.internal>
2023-02-03 02:56:38 +0000

    [Add-３]nagano_cake

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,17 @@
 class Public::ItemsController < ApplicationController
+  
   def index
+    @items = Item.page(params[:page]).per(8)
   end
 
   def show
+    @item = Item.find(params[:id])
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :is_active, :image)
+  end
+
 end
